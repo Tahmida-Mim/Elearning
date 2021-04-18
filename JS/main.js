e762b8b85e258ce6f80eb1c5ccea480b36616018
Tahmida-Mim <82760478+Tahmida-Mim@users.noreply.github.com>
2021-04-18 19:48:40 +0600

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,16 @@
+// slick slider js
+$(document).ready(function(){
+  $('.slider').slick({
+    autoplay: true ,
+    autoplaySpeed: 4000 ,
+    speed: 0,
+  });
+});
+//meanMenu js
+$(document).ready(function () {
+	$('#main_menu').meanmenu({
+    meanMenuContainer: '#mobile_menu',
+    meanScreenWidth: 991,
+  });
+});
+	
